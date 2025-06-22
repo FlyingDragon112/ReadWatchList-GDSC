d0341894838de254ab70b997ce2004e0979a303c
Himani <himanirathore508@gmail.com>
2025-06-22 21:33:40 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./LoginPage";
 import ProfilePage from "./profilepage";
+import Homepage from "./homepage";
 
 function App() {
   const params = new URLSearchParams(window.location.search);
   const email = params.get("email");
 
-  return email ? <ProfilePage email={email} /> : <MainPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/ForYou" element={<Homepage />} />
+        <Route path="/profile" element={<ProfilePage email={email} />} />
+        <Route path="*" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
